cf99455ccda945deb4023bb0e79f9112dd6ed210
gsweene2 <gsweene2@u.rochester.edu>
2015-11-06 17:50:48 -0500

    Main web template
    files for web. need to add rap phase 1 to it

---
+++ RAP/index.php
@@ -0,0 +1,146 @@
+<!doctype html>
+
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+
+    <head>
+
+        <meta charset="utf-8">
+
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+        <title></title>
+
+        <meta name="description" content="">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+
+
+
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+
+        <style>
+
+            body {
+
+                padding-top: 50px;
+
+                padding-bottom: 20px;
+
+            }
+
+        </style>
+
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+        <link rel="stylesheet" href="css/main.css">
+
+
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+
+    </head>
+
+<?php include "inc/nav.inc"; ?>
+
+    <body>
+
+        <!--[if lt IE 8]>
+
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+
+        <![endif]-->
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+
+    <div class="jumbotron">
+
+      <div class="container">
+
+        <h1>Find your rhyme!</h1>
+
+        <p>Rapbot is the cool new way to find your rhyme time lime crime mine</p>
+
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Start rapping! &raquo;</a></p>
+
+      </div>
+
+    </div>
+
+
+
+    <div class="container" style="background-color: #B99C6B;">
+
+      <!-- Example row of columns -->
+
+      <div class="row">
+
+        <div class="col-md-4">
+
+          <h2>Concept Video</h2>
+
+          <p>Click here to view our origninal concept video!</p>
+
+          <p><a class="btn btn-default" href="youtube.php" role="button">View details &raquo;</a></p>
+
+        </div>
+
+        <div class="col-md-4">
+
+          <h2>Our Progress</h2>
+
+          <p>Track our progress along the way of developing RAPBOT, a cool new software to help you find your rhyme!</p>
+
+          <p><a class="btn btn-default" href="schedule.php" role="button">View Progress &raquo;</a></p>
+
+       </div>
+
+        <div class="col-md-4">
+
+          <h2>Prototype</h2>
+
+          <p>Link to our prototype!</p>
+
+          <p><a class="btn btn-default" href="#" role="button">View prototype &raquo;</a></p>
+
+        </div>
+
+      </div>
+
+
+
+      <hr>
+
+
+
+      <footer>
+
+        <p>&copy; ASL Learning, LLC 2015</p>
+
+      </footer>
+
+    </div> <!-- /container -->        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+
+
+
+        <script src="js/vendor/bootstrap.min.js"></script>
+
+
+
+        <script src="js/main.js"></script>
+
+    </body>
+
+</html>
+
+
+
